847d3cd5e8dc48ceb0a1ae282a94ae6c5d41c780
ExquisiteCore <xiaolongqq1y@163.com>
2024-09-07 15:34:41 +0800

    fix footer

--- frontend/components/footer.vue
+++ frontend/components/footer.vue
@@ -1,20 +1,18 @@
 <template>
   <footer class="px-6 py-12">
-    <Wrapper :class="cn(
-      'grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-24 gap-y-8 text-sm text-muted-foreground',
-    )">
-      <dl class="flex flex-col gap-3">
-        <dt class="text-lg font-semibold text-primary">导航</dt>
-        <dd v-for="el in navItems" :key="el.link">
-          <NuxtLink :to="el.link" class="flex items-center transition-colors hover:font-semibold hover:text-primary">
-            {{ el.label }}
-          </NuxtLink>
-        </dd>
-      </dl>
-    </Wrapper>
 
-    <Wrapper
+    <dl class="flex flex-col gap-3">
+      <dt class="text-lg font-semibold text-primary">导航</dt>
+      <dd v-for="el in navItems" :key="el.link">
+        <NuxtLink :to="el.link" class="flex items-center transition-colors hover:font-semibold hover:text-primary">
+          {{ el.label }}
+        </NuxtLink>
+      </dd>
+    </dl>
+
+    <div
       class="flex flex-col items-center justify-center space-y-1 pt-24 text-sm text-muted-foreground md:flex-row md:space-x-4 md:space-y-0">
+
       <div class="order-3">
         <NuxtLink to="https://github.com/exquisitecore/" target="_blank" title="https://github.com/exquisitecore/"
           aria-label="https://github.com/exquisitecore/">
@@ -22,7 +20,8 @@
           ExqusiteCore
         </NuxtLink>
       </div>
-    </Wrapper>
+    </div>
+
 
   </footer>
 </template>
