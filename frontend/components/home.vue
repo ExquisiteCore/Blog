9a1ff081ef4756b23a1992ef892db0e66160767f
ExquisiteCore <xiaolongqq1y@163.com>
2024-09-07 16:53:28 +0800

    fix

--- frontend/components/home.vue
+++ frontend/components/home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex min-h-full max-w-screen-md flex-col justify-center gap-5 px-6 md:px-10 2xl:max-w-7xl">
     <p class="animate-fade-up text-2xl tracking-widest animate-ease-in-out md:text-5xl"
-      :style="{ animationDelay: `${animationDelays.p1}ms` }">
+      :style="{ animationDelay: animationDelays.p1 }">
       你好，我是
     </p>
 
@@ -10,20 +10,20 @@
       'animate-fade-up animate-ease-in-out'
     )" :style="{
       WebkitTextFillColor: 'transparent',
-      animationDelay: `${animationDelays.strong}ms`
+      animationDelay: animationDelays.strong
     }">
       ExquisiteCore
     </strong>
 
-    <div :class="cn('animate-fade-up animate-ease-in-out')"
-      :style="{ animationDelay: `${animationDelays.typeIntro}ms` }">
+    <div v-if="isMounted" :class="cn('animate-fade-up animate-ease-in-out')"
+      :style="{ animationDelay: animationDelays.typeIntro }">
       <TypeIntro />
     </div>
 
     <p :class="cn(
       'text-2xl md:text-5xl tracking-widest',
       'animate-fade-up animate-ease-in-out'
-    )" :style="{ animationDelay: `${animationDelays.p2}ms` }">
+    )" :style="{ animationDelay: animationDelays.p2 }">
       喜欢
       <span class="font-semibold text-[#f44c7c]">C++</span>、
       <span class="font-semibold text-[#dca484]">Rust</span>和
@@ -35,43 +35,39 @@
     <p :class="cn(
       'text-base md:text-2xl text-muted-foreground tracking-widest',
       'animate-fade-up animate-ease-in-out',
-    )" :style="{ animationDelay: `${animationDelays.p3}ms` }">
+    )" :style="{ animationDelay: animationDelays.p3 }">
       欢迎来到我的个人网站，这里记录了一些关于我的技术之路、生活之道、感悟之事。
     </p>
 
-    <div :class="cn('flex space-x-4', 'animate-fade-up animate-ease-in-out')"
-      :style="{ animationDelay: `${animationDelays.buttons}ms` }">
+    <div v-if="isMounted" :class="cn('flex space-x-4', 'animate-fade-up animate-ease-in-out')"
+      :style="{ animationDelay: animationDelays.buttons }">
       <NuxtLink to="/blog">
-        <el-button class="w-15 h-15">我的博客</el-button>
+        <el-button class="w-20 h-10">我的博客</el-button>
       </NuxtLink>
       <NuxtLink to="/about">
-        <el-button class="w-15 h-15">关于我</el-button>
+        <el-button class="w-20 h-10">关于我</el-button>
       </NuxtLink>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from 'vue'
+import { computed, ref, onMounted } from 'vue'
 
-// 定义初始延迟值
 const baseDelay = 200
+const delay = ref(baseDelay * 5) // 初始化 delay 值
+const isMounted = ref(false)
 
-// 响应式变量用于跟踪当前的延迟值
-const delay = ref(0)
-
-// 计算属性用于获取不同元素的动画延迟
-const animationDelays = computed(() => {
-  // 延迟值
-  delay.value += baseDelay
-  return {
-    p1: delay.value,
-    strong: delay.value + baseDelay,
-    typeIntro: delay.value + baseDelay * 2,
-    p2: delay.value + baseDelay * 3,
-    p3: delay.value + baseDelay * 4,
-    buttons: delay.value + baseDelay * 5
-  }
+onMounted(() => {
+  isMounted.value = true
 })
 
+const animationDelays = computed(() => ({
+  p1: `${delay.value}ms`,
+  strong: `${delay.value + baseDelay}ms`,
+  typeIntro: `${delay.value + baseDelay * 2}ms`,
+  p2: `${delay.value + baseDelay * 3}ms`,
+  p3: `${delay.value + baseDelay * 4}ms`,
+  buttons: `${delay.value + baseDelay * 5}ms`
+}))
 </script>
--- frontend/components/navbar.vue
+++ frontend/components/navbar.vue
@@ -1,11 +1,11 @@
 <template>
   <header :class="cn(
     'w-full sticky top-0 backdrop-blur transition-[background-color,border-width] border-x-0 flex justify-center z-10',
-    (scrollTop > 60) && 'bg-background/50 border-b border-border/50'
+    scrollTop > 60 ? 'bg-background/50 border-b border-border/50' : ''
   )">
     <div class="flex h-16 w-full items-center p-4 sm:p-8 md:max-w-screen-md 2xl:max-w-screen-xl">
 
-      <NuxtLink to="/" :class="cn('mr-4 hidden sm:flex')" aria-label="EC">
+      <NuxtLink to="/" :class="cn('mr-4 hidden sm:flex')" aria-label=" EC">
         <p class="font-bold text-2xl">EC</p>
         <span class="ml-2 text-base font-semibold text-primary">
           精致的芯
@@ -15,7 +15,7 @@
         <NuxtLink v-for="el in navItems" :key="el.link" :to="el.link" :class="cn(
           'font-normal text-sm text-muted-foreground transition-colors px-4 py-2',
           'hover:font-semibold hover:text-primary',
-          pathname === el.link && 'font-semibold text-primary'
+          route.path === el.link && 'font-semibold text-primary'
         )">
           {{ el.label }}
         </NuxtLink>
@@ -32,30 +32,11 @@
   </header>
 </template>
 <script setup lang="ts">
-import { ref, onMounted, onUnmounted } from 'vue'
+import { ref, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
 
-// 获取当前路径
-const route = useRoute();
-const pathname = route.path;
-
-// 创建 scrollTop 状态
-const scrollTop = ref(0);
-
-// 监听页面滚动
-const handleScroll = () => {
-  scrollTop.value = window.scrollY;
-};
-
-// 在组件挂载时监听滚动事件
-onMounted(() => {
-  window.addEventListener('scroll', handleScroll);
-});
-
-// 在组件卸载时移除监听
-onUnmounted(() => {
-  window.removeEventListener('scroll', handleScroll);
-});
-
+const scrollTop = ref(0)
+const route = useRoute()
 
 const navItems = [
   { label: '首页', link: '/' },
@@ -63,4 +44,9 @@ const navItems = [
   { label: '关于', link: '/about' }
 ];
 
+onMounted(() => {
+  window.addEventListener('scroll', () => {
+    scrollTop.value = window.scrollY
+  })
+})
 </script>
