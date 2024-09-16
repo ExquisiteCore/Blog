966fbd20b6d99ed39e5f18cc3ff5224354106f62
玲珑芯ExquisiteCore <59426890+ExquisiteCore@users.noreply.github.com>
2024-09-17 02:34:41 +0800

    更新 index.vue

--- frontend/pages/blog/index.vue
+++ frontend/pages/blog/index.vue
@@ -52,7 +52,7 @@ interface FetchData {
 }
 const posts = ref<any[]>([])
 //使用 useFetch 从 API 获取文章数据
-const { data, error } = await useFetch<FetchData>('http://rack1.raincs.cc:56394/api/post/posts')
+const { data, error } = await useFetch<FetchData>('https://blogserver.exquisitecore.xyz/api/post/posts')
 // 检查是否有错误
 if (error.value) {
   console.error('获取文章失败', error.value)
