1a4bb811c5c9b8fb956090d1f491471bfc0fb226
ExquisiteCore <xiaolongqq1y@163.com>
2024-09-24 00:52:50 +0800

    add md and ml

--- frontend/pages/blog/article.vue
+++ frontend/pages/blog/article.vue
@@ -1,30 +1,44 @@
 <template>
-  <div class="flex flex-col items-center max-w-screen-lg mx-auto px-6 py-12">
-    <!-- 图片展示 -->
-    <div class="flex justify-center mb-8">
-      <img :src="getPreviewImage(postData?.Preview || '')" alt="文章图片"
-        class="w-72 h-72 object-cover rounded-lg shadow-lg">
-    </div>
-    <!-- 文章标题 -->
-    <h1 class="text-4xl font-bold text-center mb-4">{{ postData?.Title }}</h1>
-    <!-- 显示文章数据 -->
+  <div>
+    <div class="flex flex-col items-center max-w-screen-lg mx-auto px-6 py-12">
+      <!-- 图片展示 -->
+      <div class="flex justify-center mb-8">
+        <img :src="getPreviewImage(postData?.Preview || '')" alt="文章图片"
+          class="w-72 h-72 object-cover rounded-lg shadow-lg">
+      </div>
+      <!-- 文章标题 -->
+      <h1 class="text-4xl font-bold text-center mb-4">{{ postData?.Title }}</h1>
+      <!-- 显示文章数据 -->
 
-    <!-- 标签和字数统计 -->
-    <div class="flex justify-center items-center space-x-4 mb-8">
-      Tags
-      <span v-for="(tag, index) in postData?.Tags" :key="index"
-        class="bg-orange-400 text-white px-3 py-1 rounded-full text-sm">
-        {{ tag }}
-      </span>
+      <!-- 标签和字数统计 -->
+      <div class="flex justify-center items-center space-x-4 mb-8">
+        Tags
+        <span v-for="(tag, index) in postData?.Tags" :key="index"
+          class="bg-orange-400 text-white px-3 py-1 rounded-full text-sm">
+          {{ tag }}
+        </span>
+      </div>
     </div>
-    <!-- 文章内容 -->
-    <MdPreview v-model="renderedContent" previewTheme="github" editorId="article-markdown-editor" />
+    <!-- 文章内容和目录 -->
+    <div class="flex justify-between w-full">
+      <div class="flex-1">
+        <MdPreview v-model="renderedContent" previewTheme="arknights" :editorId="id" />
+      </div>
+      <!-- 竖线分隔符 -->
+      <div class="w-px bg-gray-300 mx-4"></div>
+      <div class="flex-none sticky top-20 h-screen overflow-auto">
+        <MdCatalog :editorId="id" scrollElement="html" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { useRoute } from 'vue-router';
-import { MdPreview } from 'md-editor-v3';
+import { MdPreview, MdCatalog } from 'md-editor-v3';
+
+import '@vavt/cm-extension/dist/previewTheme/arknights.css';
+const id = 'article-markdown-editor';
 
 const route = useRoute();
 const seq = route.query.seq || '';
