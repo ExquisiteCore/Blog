51648cdfc1f66525e3a65deea9d3d5ecaa70eb5f
ExquisiteCore <xiaolongqq1y@163.com>
2025-03-20 18:42:25 +0800

    refactor(home): 将 HomeSection 组件从箭头函数改为函数声明
    为了提高代码的可读性和一致性，将 HomeSection 组件的定义从箭头函数改为函数声明。这种修改不影响组件的行为，但使代码更符合团队编码规范。

--- frontend/src/components/home/index.tsx
+++ frontend/src/components/home/index.tsx
@@ -1,8 +1,7 @@
 import { Images } from "../images";
 import { Brand } from "../brand";
 
-export const HomeSection = () => {
-
+export function HomeSection() {
   return (
     <div className="flex min-h-full  max-w-screen-md flex-col justify-center gap-5 px-6 md:px-10 2xl:max-w-7xl">
       <Images />
