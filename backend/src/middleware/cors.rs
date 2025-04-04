d0dd912a0bd56620f6403c54f7a5181e92fe9e68
ExquisiteCore <xiaolongqq1y@163.com>
2025-04-04 09:10:09 +0800

    refactor(middleware/cors): 优化跨域中间件配置
    将允许的方法、头部和源从`Any`替换为具体的值，并启用凭据支持，以提高安全性和明确性

--- backend/src/middleware/cors.rs
+++ backend/src/middleware/cors.rs
@@ -1,9 +1,32 @@
-use tower_http::cors::{Any, CorsLayer};
+use axum::http;
+use http::Method;
+use http::header::{AUTHORIZATION, CONTENT_TYPE};
+use tower_http::cors::CorsLayer;
 
 /// 创建跨域中间件
 pub fn create_layer() -> CorsLayer {
+    // 定义允许的源
+    let origins = [
+        "http://localhost:3000".parse().unwrap(),
+        "https://exquisitecore.xyz".parse().unwrap(),
+        "https://www.exquisitecore.xyz".parse().unwrap(),
+    ];
+
+    // 定义允许的方法
+    let methods = [
+        Method::GET,
+        Method::POST,
+        Method::PUT,
+        Method::DELETE,
+        Method::OPTIONS,
+    ];
+
+    // 定义允许的头部
+    let headers = [AUTHORIZATION, CONTENT_TYPE];
+
     CorsLayer::new()
-        .allow_methods(Any)
-        .allow_headers(Any)
-        .allow_origin(Any)
+        .allow_methods(methods)
+        .allow_headers(headers)
+        .allow_origin(origins)
+        .allow_credentials(true)
 }
