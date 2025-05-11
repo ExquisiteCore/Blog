e309df66d6da5c39cc7866ec378b930298c36cb1
ExquisiteCore <xiaolongqq1y@163.com>
2025-05-11 22:15:36 +0800

    ssr

--- frontend-dev/astro.config.mjs
+++ frontend-dev/astro.config.mjs
@@ -3,9 +3,19 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import solidJs from '@astrojs/solid-js';
+
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [solidJs()],
+
+  adapter: node({
+    mode: 'standalone'
+  })
 });
