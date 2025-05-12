2f458d8980a08639bfe03c89268d2cf386f9fc2e
ExquisiteCore <xiaolongqq1y@163.com>
2025-05-12 22:20:04 +0800

    feat: 添加 Solid.js 支持并优化 Header 组件
    - 在项目中集成 Solid.js 及其相关依赖
    - 配置 tsconfig.json 和 astro.config.mjs 以支持 Solid.js
    - 优化 Header 组件，添加元素存在性检查

--- blog-web/astro.config.mjs
+++ blog-web/astro.config.mjs
@@ -3,9 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import solidJs from '@astrojs/solid-js';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [solidJs()]
 });
